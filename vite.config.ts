6b211e8bbb62a45736754df267b1b9590bed588f
sureshkgc <sureshkgc@gmail.com>
2025-12-19 19:58:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/SSL-Website/",
+  base: "/SSL-Website/", // must match your repo name
   plugins: [react()],
 })
